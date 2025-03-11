f4c88c5a647c660f141b401a79a49ad94bb36917
Dhivya Sreedhar <dhivyasreedhar@gmail.com>
2025-03-11 11:47:18 -0400

    Create Sort Colors.cpp

---
+++ Arrays/Sort Colors.cpp
@@ -0,0 +1,17 @@
+#TC: O(N)
+#TC: O(1)
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=0,mid=0,h=nums.size()-1;
+        while(mid<=h){
+            if(nums[mid]==0) {swap(nums[l],nums[mid]);l++;mid++;}
+            else if(nums[mid]==1)
+            mid++;
+            else if(nums[mid]==2)
+            {swap(nums[mid],nums[h]);h--;}
+        }
+        
+    }
+};
